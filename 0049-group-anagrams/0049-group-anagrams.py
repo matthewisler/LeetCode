10d5fd05dbf7bae3846257036110e7cc349c696a
matthewisler <100082718+matthewisler@users.noreply.github.com>
2024-12-05 15:23:52 -0500

    Time: 24 ms (45.81%), Space: 18 MB (5.3%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        ans = defaultdict(list)
+        for word in strs:
+            count = [0] * 26
+            for ch in word:
+                count[ord(ch) - ord('a')] += 1
+            ans[tuple(count)].append(word)
+        return list(ans.values())
