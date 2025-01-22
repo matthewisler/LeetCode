76ae6d7ecbfdea24eec5e83587404eba39621b0d
matthewisler <100082718+matthewisler@users.noreply.github.com>
2025-01-22 18:19:38 -0500

    Time: 2 ms (68.1%), Space: 17.9 MB (16.71%) - LeetHub

---
+++ 1297-maximum-number-of-balloons/1297-maximum-number-of-balloons.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        from collections import Counter
+        counter = Counter(text)
+        print(counter)
+        balloon_set = set("balloon")
+        if len(balloon_set & counter.keys()) != 5:
+            return 0
+        
+        counter['l'] //= 2
+        counter['o'] //= 2
+
+        return min(counter['b'],counter['a'],counter['l'],counter['o'],counter['n'])
