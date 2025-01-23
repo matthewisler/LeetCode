0d369d4d43447e2f8a2ad93e810020dde98fe70a
matthewisler <100082718+matthewisler@users.noreply.github.com>
2025-01-23 11:27:10 -0500

    Time: 19 ms (82.04%), Space: 17 MB (36.99%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -4,10 +4,8 @@ class Solution(object):
         :type strs: List[str]
         :rtype: List[List[str]]
         """
+        from collections import defaultdict
         ans = defaultdict(list)
         for word in strs:
-            count = [0] * 26
-            for ch in word:
-                count[ord(ch) - ord('a')] += 1
-            ans[tuple(count)].append(word)
+            ans[tuple(sorted(word))].append(word)
         return list(ans.values())
