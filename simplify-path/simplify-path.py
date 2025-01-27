d57faa8bfba94db2789a7f69eca6dbb1ef12837c
matthewisler <100082718+matthewisler@users.noreply.github.com>
2025-01-27 13:40:44 -0500

    Time: 3 ms, Memory: 17.6 MB - LeetHub

---
+++ simplify-path/simplify-path.py
@@ -0,0 +1,17 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        list_vals = path.split("/")
+        stack = []
+        for directory in list_vals:
+            if directory == ".":
+                continue
+            elif stack and directory == "..":
+                stack.pop()
+            else:
+                if directory == "..":
+                    continue
+                if len(directory) > 0:
+                    stack.append(directory)
+
+        stack = [x for x in stack if len(x)>0]
+        return '/'+'/'.join(stack)
