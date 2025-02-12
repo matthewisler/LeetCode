ec5a6c081863726d4e2231d449948bf35890a5d4
matthewisler <100082718+matthewisler@users.noreply.github.com>
2025-02-12 11:47:52 -0500

    Time: 0 ms (100%), Space: 18.2 MB (28.99%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.py
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.py
@@ -15,15 +15,17 @@ class Solution:
 
         while queue:
             queue_length = len(queue)
-            #do something per level
             depth += 1
             this_depth = []
+
             for _ in range(queue_length):
                 node = queue.popleft()
+
                 if depth % 2 == 0:
                     this_depth.append(node.val)
                 else:
                     this_depth = [node.val] + this_depth
+                    
                 if node.left:
                     queue.append(node.left)
                 if node.right:
