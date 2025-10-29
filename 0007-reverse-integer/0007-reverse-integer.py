eefdf35ff8e1b1814cab151380319475027d5838
Matt <100082718+matthewisler@users.noreply.github.com>
2025-10-29 14:47:34 -0400

    Time: 33 ms (87.44%), Space: 18.1 MB (17.03%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -0,0 +1,22 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        neg = False
+        if x:
+            if '-' in str(x):
+                neg = True
+                x *= -1
+
+            x_list = [str(num) for num in str(x)]
+            print(x_list)
+            output = int(''.join(x_list[::-1]))
+            if neg:
+                output *= -1
+
+            if output > (2**31) - 1 or output < -2**31:
+                return 0
+            
+            
+            return output
+            
+        else:
+            return 0
