22d61e96234c856f02ae3e2e6ddef70024980d06
Matt <100082718+matthewisler@users.noreply.github.com>
2025-11-02 11:36:54 -0500

    Time: 454 ms (78.12%), Space: 0B (100%) - LeetHub

---
+++ 1180-game-play-analysis-ii/1180-game-play-analysis-ii.sql
@@ -0,0 +1,16 @@
+# Write your MySQL query statement below
+SELECT
+  A1.player_id,
+  A1.device_id
+FROM
+  Activity A1
+WHERE
+  (A1.player_id, A1.event_date) IN (
+    SELECT
+      A2.player_id,
+      MIN(A2.event_date)
+    FROM
+      Activity A2
+    GROUP BY
+      A2.player_id
+  )
