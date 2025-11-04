d6445ad08c9012c36a6437011270eccce7cde0ae
Matt <100082718+matthewisler@users.noreply.github.com>
2025-11-03 21:24:39 -0500

    Time: 1054 ms (21.21%), Space: 0B (100%) - LeetHub

--- 1551-npv-queries/1551-npv-queries.sql
+++ 1551-npv-queries/1551-npv-queries.sql
@@ -1,9 +1,10 @@
 # Write your MySQL query statement below
 SELECT 
-  Q.id, 
-  Q.year, 
-  IFNULL(N.npv, 0) AS npv 
+    Q.id, 
+    Q.year, 
+    IFNULL(N.npv, 0) AS npv 
 FROM 
-  Queries Q 
-  LEFT JOIN NPV N ON Q.id = N.id 
-  AND Q.year = N.year;
+    Queries Q 
+LEFT JOIN 
+    NPV N 
+ON Q.id = N.id AND Q.year = N.year;
