50cd049ee4a5d9b5772006d514f7be1f0b9f764b
Matt <100082718+matthewisler@users.noreply.github.com>
2025-11-06 12:45:02 -0500

    Time: 404 ms (5.03%), Space: 18.6 MB (20.6%) - LeetHub

--- 0274-h-index/0274-h-index.py
+++ 0274-h-index/0274-h-index.py
@@ -1,11 +1,7 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
         output = 0
-        if len(citations) == 1:
-            if citations[0] > 0:
-                return 1
-            else:
-                return 0
+        
 
         for i in range(1, len(citations)+1):
             cites = [x for x in citations if i<=x]
