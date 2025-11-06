db837f1c4220a46ba04b321ad32e7d1e77291909
Matt <100082718+matthewisler@users.noreply.github.com>
2025-11-06 12:51:30 -0500

    Time: 488 ms (67.65%), Space: 0B (100%) - LeetHub

---
+++ 1747-leetflex-banned-accounts/1747-leetflex-banned-accounts.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+SELECT
+    DISTINCT l1.account_id
+FROM
+    LogInfo l1
+CROSS JOIN
+    LogInfo l2
+WHERE
+    l1.account_id = l2.account_id AND
+    l1.ip_address != l2.ip_address AND
+    l1.login <= l2.logout AND l2.login <= l1.logout
