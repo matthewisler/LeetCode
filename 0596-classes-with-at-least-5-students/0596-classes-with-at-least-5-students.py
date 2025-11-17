74b4e007ba9031c53dca51a06790839bfab0d02c
Matt <100082718+matthewisler@users.noreply.github.com>
2025-11-17 12:27:07 -0500

    Time: 249 ms (92.85%), Space: 68.2 MB (49.67%) - LeetHub

--- 0596-classes-with-at-least-5-students/0596-classes-with-at-least-5-students.py
+++ 0596-classes-with-at-least-5-students/0596-classes-with-at-least-5-students.py
@@ -3,4 +3,4 @@ import pandas as pd
 def find_classes(courses: pd.DataFrame) -> pd.DataFrame:
     df = courses.groupby('class').count().reset_index()
     df = df[df['student']>=5]
-    return df['class'].to_frame()
+    return df[['class']]
