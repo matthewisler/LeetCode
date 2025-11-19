b1d2f1eccbe16ce67025117a1531f69009555722
Matt <100082718+matthewisler@users.noreply.github.com>
2025-11-19 15:44:40 -0500

    Time: 329 ms (6.89%), Space: 67.5 MB (27.54%) - LeetHub

---
+++ 0182-duplicate-emails/0182-duplicate-emails.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+def duplicate_emails(person: pd.DataFrame) -> pd.DataFrame:
+    df = person.groupby('email').count().reset_index()
+    df = df[df['id']>1]
+    df = df.rename(columns={"email":"Email"})
+    print(df.head())
+    return df['Email'].to_frame()
